5422e6d2dfc41dcf5a096f682fc4dc75b3605d5e
khramov23 <79625109971@ya.ru>
2023-01-30 20:43:59 +0300

    add theming

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,18 @@
 import React, {Suspense} from 'react';
 
-import '../index.scss'
+import '../styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "../pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "../pages/MainPage/MainPage.async";
+import {useTheme} from "../theme/useTheme";
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to='/'>Главная</Link>
             <Link to='/about'>О сайте</Link>
             <Suspense fallback={<h1>Loading...</h1>}>
@@ -16,7 +21,6 @@ const App = () => {
                     <Route path='/' element={<MainPageAsync/>}/>
                 </Routes>
             </Suspense>
-
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import {render} from "react-dom";
 import App from "./components/App";
 import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ThemeProvider from "./theme/ThemeProvider";
 
 
 render(
     <BrowserRouter>
-        <App />
+        <ThemeProvider>
+            <App/>
+        </ThemeProvider>
     </BrowserRouter>,
     document.getElementById('root')
 )
